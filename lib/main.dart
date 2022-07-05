20bc1b07dd2aef839e10238e9b0673b4871479db
Avighna Jha <avighnajha1703@gmail.com>
2022-07-05 23:03:35 +0530

    Cleaned up code and made several widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,13 @@
-import 'dart:html';
-
 import "package:flutter/material.dart";
+
+import './product_manager.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    return _MyAppState();
-  }
-}
-
-class _MyAppState extends State<MyApp> {
+//How stateful widgets work in flutter -- look into it a bit more
+class MyApp extends StatelessWidget {
   @override // This just acts as an annotation telling us that this block of code just overides statelesswidget
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -21,25 +15,7 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: Text("EasyList"),
         ),
-        body: Column(
-          children: [
-            Container(
-              margin: EdgeInsets.all(10.0),
-              child: RaisedButton(
-                onPressed: () {},
-                child: Text("Add Product"),
-              ),
-            ),
-            Card(
-              child: Column(
-                children: [
-                  Image.asset("assets/food.jpg"),
-                  Text("Food Paradise")
-                ],
-              ),
-            ),
-          ],
-        ),
+        body: ProductManager(),
       ),
     );
   }
