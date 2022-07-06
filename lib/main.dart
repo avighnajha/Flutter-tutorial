94c4cbc68359f42a04f3aa051d308c51707172ec
Avighna Jha <avighnajha1703@gmail.com>
2022-07-06 22:09:57 +0530

    End of tutorial

--- lib/main.dart
+++ lib/main.dart
@@ -11,11 +11,17 @@ class MyApp extends StatelessWidget {
   @override // This just acts as an annotation telling us that this block of code just overides statelesswidget
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+          brightness: Brightness.light,
+          primarySwatch: Colors.deepOrange,
+          accentColor: Colors.deepPurple),
       home: Scaffold(
         appBar: AppBar(
           title: Text("EasyList"),
         ),
-        body: ProductManager(),
+        body: ProductManager(
+          startingProduct: "Food Tester",
+        ),
       ),
     );
   }
